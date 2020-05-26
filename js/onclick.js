ec46568a1e764588b494f0955c082bdf57fd7e40
mishel21 <vyhrynovskiy@gmail.com>
2020-05-26 17:14:53 +0300

    v2

---
+++ js/onclick.js
@@ -0,0 +1,9 @@
+// let b1 = document.getElementById("b1");
+// b1.onclick = () => {
+//     b1.style.backgroundColor = "black";
+//
+// };
+// let b2 = document.getElementById("b2");
+// b2.onclick = () => {
+//     b2.style.backgroundColor = "black"
+// };
